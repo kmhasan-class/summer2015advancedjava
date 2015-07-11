3b688dd12a0e072cf241b47a6af77823d781f4d2
kmhasan <kmhasan@gmail.com>
2015-07-12 00:50:14 +0600

    Simple demo to show git branching

---
+++ Git Demo/src/bd/ac/seu/GitDemo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd.ac.seu;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class GitDemo {
+    public static int add(int a, int b) {
+        return 0;
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(add(3, 4));
+    }
+    
+}
